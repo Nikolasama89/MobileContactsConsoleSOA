fb1fc8a070e971beb3e22e12623e4a52d3ed5a3c
Nikos <niqos89@gmail.com>
2025-01-27 19:02:07 +0200

    Initial commit

---
+++ src/gr/aueb/cf/mobilecontacts/dao/IMobileContactDAO.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.mobilecontacts.dao;
+
+
+
+import gr.aueb.cf.mobilecontacts.model.MobileContact;
+
+import java.util.List;
+
+public interface IMobileContactDAO {
+    //Basic CRUD
+    MobileContact insert(MobileContact mobileContact);
+    MobileContact update(Long id, MobileContact mobileContact);
+    void deleteById(Long id);
+    MobileContact getById(Long id);
+    List<MobileContact> getAll();
+
+    void deleteByPhoneNumber(String phoneNumber);
+
+    // Queries
+    MobileContact getByPhoneNumber(String phoneNumber);
+    boolean userIDExists(Long id);
+    boolean phoneNumberExists(String phoneNumber);
+
+
+}
